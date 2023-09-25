f69f1e42169c924abe3413122b5f25d4d4a8e4ff
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2023-09-25 19:15:53 -0400

    describe table and table keys

--- pkg/dynamodb/dynamodb.go
+++ pkg/dynamodb/dynamodb.go
@@ -14,6 +14,39 @@ type ListTable struct {
 	TableNames []string `json:"TableNames"`
 }
 
+type DescribeTableResponse struct {
+	Table Table `json:"Table"`
+}
+
+type Table struct {
+	AttributeDefinitions  []AttributeDefinition `json:"AttributeDefinitions"`
+	CreationDateTime      int64                 `json:"CreationDateTime"`
+	ItemCount             int                   `json:"ItemCount"`
+	KeySchema             []KeySchemaElement    `json:"KeySchema"`
+	ProvisionedThroughput ProvisionedThroughput `json:"ProvisionedThroughput"`
+	TableName             string                `json:"TableName"`
+	TableSizeBytes        int                   `json:"TableSizeBytes"`
+	TableStatus           string                `json:"TableStatus"`
+}
+
+type AttributeDefinition struct {
+	AttributeName string `json:"AttributeName"`
+	AttributeType string `json:"AttributeType"`
+}
+
+type KeySchemaElement struct {
+	AttributeName string `json:"AttributeName"`
+	KeyType       string `json:"KeyType"`
+}
+
+type ProvisionedThroughput struct {
+	LastDecreaseDateTime   int64 `json:"LastDecreaseDateTime"`
+	LastIncreaseDateTime   int64 `json:"LastIncreaseDateTime"`
+	NumberOfDecreasesToday int   `json:"NumberOfDecreasesToday"`
+	ReadCapacityUnits      int   `json:"ReadCapacityUnits"`
+	WriteCapacityUnits     int   `json:"WriteCapacityUnits"`
+}
+
 func ListTables(aws aws.Aws) []string {
 	cmd := exec.Command("aws", "dynamodb", "list-tables", "--profile", aws.Profile, "--region", aws.Region)
 	out, err := cmd.Output()
@@ -61,4 +94,68 @@ func PutItem(aws aws.Aws, table string, pathToItem string) {
 		log.Fatal(err)
 	}
 	fmt.Println(string(out))
+}
+
+func DescribeTable(aws aws.Aws, table string) DescribeTableResponse {
+	cmd := exec.Command("aws", "dynamodb", "describe-table", "--table-name", table, "--profile", aws.Profile, "--region", aws.Region)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	DescribeTableResponse := DescribeTableResponse{}
+	json.Unmarshal(out, &DescribeTableResponse)
+	return DescribeTableResponse
+}
+
+func TableKeysFromTableDescription(tableDescription DescribeTableResponse) (string, string) {
+	partitionKey := ""
+	sortKey := ""
+	for _, key := range tableDescription.Table.KeySchema {
+		if partitionKey != "" && sortKey != "" {
+			break
+		}
+		if key.KeyType == "HASH" {
+			partitionKey = key.AttributeName
+		} else if key.KeyType == "RANGE" {
+			sortKey = key.AttributeName
+		}
+	}
+	return partitionKey, sortKey
+}
+
+func TableKeys(aws aws.Aws, table string) (string, string) {
+	tableDescription := DescribeTable(aws, table)
+	partitionKey := ""
+	sortKey := ""
+	for _, key := range tableDescription.Table.KeySchema {
+		if partitionKey != "" && sortKey != "" {
+			break
+		}
+		if key.KeyType == "HASH" {
+			partitionKey = key.AttributeName
+		} else if key.KeyType == "RANGE" {
+			sortKey = key.AttributeName
+		}
+	}
+	return partitionKey, sortKey
+}
+
+func TablePartitionKey(aws aws.Aws, table string) string {
+	tableDescription := DescribeTable(aws, table)
+	for _, key := range tableDescription.Table.KeySchema {
+		if key.KeyType == "HASH" {
+			return key.AttributeName
+		}
+	}
+	return ""
+}
+
+func TableSortKey(aws aws.Aws, table string) string {
+	tableDescription := DescribeTable(aws, table)
+	for _, key := range tableDescription.Table.KeySchema {
+		if key.KeyType == "RANGE" {
+			return key.AttributeName
+		}
+	}
+	return ""
 }
