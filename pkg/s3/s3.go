e51d2726c8fa378d1e99c0030cc9b736b4c29f5c
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2023-09-28 08:27:59 -0400

    add ListBucketsSDK

--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -1,9 +1,13 @@
 package s3
 
 import (
+	"context"
 	"log"
 	"os/exec"
 	"strings"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func ListBuckets(profile string) []string {
@@ -18,5 +22,22 @@ func ListBuckets(profile string) []string {
 		buckets[i] = strings.Split(bucket, " ")[2]
 	}
 
+	return buckets
+}
+
+func ListBucketsSDK(profile string) []string {
+	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile))
+	if err != nil {
+		panic(err)
+	}
+	s3Client := s3.NewFromConfig(sdkConfig)
+	result, err := s3Client.ListBuckets(context.TODO(), &s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+	var buckets []string
+	for _, bucket := range result.Buckets {
+		buckets = append(buckets, *bucket.Name)
+	}
 	return buckets
 }
