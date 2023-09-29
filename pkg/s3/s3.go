39788f19e8907e35a4278ebe9bdfa7aad245aad8
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2023-09-29 09:34:48 -0400

    remove cli list buckets in favor of sdk

--- pkg/s3/s3.go
+++ pkg/s3/s3.go
@@ -2,30 +2,12 @@ package s3
 
 import (
 	"context"
-	"log"
-	"os/exec"
-	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func ListBuckets(profile string) []string {
-	s3Cmd := exec.Command("aws", "s3", "ls", "--profile", profile)
-	out, err := s3Cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	buckets := strings.Split(string(out), "\n")
-	buckets = buckets[:len(buckets)-1]
-	for i, bucket := range buckets {
-		buckets[i] = strings.Split(bucket, " ")[2]
-	}
-
-	return buckets
-}
-
-func ListBucketsSDK(profile string) []string {
 	sdkConfig, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile))
 	if err != nil {
 		panic(err)
